52bbc81c5cda145fe08ea5e39f5ba862432c2431
Ohtaro654 <184686837+Ohtaro654@users.noreply.github.com>
2025-01-06 21:30:43 +0100

    class voor auto's toegevoegd

---
+++ rush-hour.py
@@ -0,0 +1,31 @@
+class auto():
+    def __init__(self, positie, lengte, naam, ligging):
+        # Coördinaat x en y tuple
+        self.positie = positie
+        self.lengte = lengte
+        self.naam = naam
+        # Horizontaal of verticaal
+        self.ligging = ligging
+
+    def beweeg_auto(self, richting):
+        row, col = self.positie
+        # Ligging horizontaal
+        if self.ligging == 'H':
+            if richting == 'Links':
+                col += 1
+            elif richting == 'Rechts':
+                col -= 1
+            else:
+                raise ValueError("Ongeldige richting voor horizontale auto!")
+
+        # Ligging verticaal
+        elif self.ligging == 'V':
+            if richting == 'Boven':
+                row += 1
+            elif richting == 'Onder':
+                row -= 1
+            else:
+                raise ValueError("Ongeldige richting voor verticale auto!")
+        # Update positie
+        self.positie = (row, col)
+
