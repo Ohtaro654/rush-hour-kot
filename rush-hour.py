dd9494a37d31343b58fcd3ad69364a5793bf4bcc
Ohtaro654 <184686837+Ohtaro654@users.noreply.github.com>
2025-01-07 10:58:40 +0100

    grid class toegevoegd, nu is er een grid gemaakt met de functie om auto's toe te voegen

--- rush-hour.py
+++ rush-hour.py
@@ -31,7 +31,31 @@ class auto():
         
 class grid():
     def __init__(self):
+        # Maakt grid van 6x6 met _
         self.grid = [['_'] * 6 for _ in range(6)]
 
     def toevoeg_auto(self, auto):
+        row, col = auto.positie
+        if auto.ligging == 'H':
+            # Kijken of auto past op bord
+            if col + auto.lengte > 6:
+                raise ValueError("Past niet op het bord!")
+            # Kijken of niet overlapt
+            for i in range(col, col + auto.lengte):
+                if self.grid[row][i] != '_':
+                    raise ValueError("Auto overlapt!")
+            # Voeg auto toe
+            for i in range(col, col + auto.lengte):
+                self.grid[row][i] = auto.naam
 
+        elif auto.ligging == 'V':
+            # Kijken of auto past op bord
+            if row + auto.lengte > 6:
+                raise ValueError("Past niet op het bord!")
+            # Kijken of niet overlapt
+            for j in range(row, row + auto.lengte):
+                if self.grid[j][col] != '_':
+                    raise ValueError("Auto overlapt!")
+            # Voeg auto toe
+            for j in range(row, row + auto.lengte):
+                self.grid[j][col] = auto.naam
