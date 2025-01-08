d0536efbd2550433a6233a9daaa9f2761db82867
Ohtaro654 <184686837+Ohtaro654@users.noreply.github.com>
2025-01-08 14:51:13 +0100

    tarik deelt toegevoegd (credit naar Tarik)

--- rush-hour.py
+++ rush-hour.py
@@ -1,13 +1,16 @@
-class auto():
+import os
+import csv
+
+class Auto():
     def __init__(self, row, col, lengte, naam, ligging):
         # Coördinaat x en y tuple
-        self.positie = (row, col)
+        self.positie = (row - 1, col - 1)
         self.lengte = lengte
         self.naam = naam
         # Horizontaal of verticaal
         self.ligging = ligging
 
-class grid():
+class Grid():
     '''
     Variabele size (voor grid) toevoegen.
     '''
@@ -21,8 +24,6 @@ class grid():
         '''
     def toevoeg_auto(self, auto):
         row, col = auto.positie
-        row -= 1
-        col -= 1
         if auto.ligging == 'H':
             # Kijken of auto past op bord
             if col + auto.lengte > self.size:
@@ -46,7 +47,11 @@ class grid():
             # Voeg auto toe
             for j in range(auto.lengte):
                 self.grid[row + j][col] = auto.naam
-                
+        
+    def toon_bord(self):
+        for row in self.grid:
+            print(' '.join(row))
+    
         '''
         Finish maken
         '''
@@ -56,8 +61,6 @@ class grid():
     '''
     def beweeg_auto(self, auto, richting, stapgrootte):
         row, col = auto.positie
-        row -= 1
-        col -= 1
         
         # Ligging horizontaal
         if auto.ligging == 'H':
@@ -91,10 +94,10 @@ class grid():
                 if row - stapgrootte >= 0 and all(self.grid[row - i][col] == '_' for i in range(1, stapgrootte + 1)):
                     for i in range(auto.lengte):
                         self.grid[row - stapgrootte + i][col] = auto.naam
-                    for i in range(stapgrootte)
-                    self.grid[row + auto.lengte - i - 1][col] = '_'
-                    row -= stapgrootte
-                    
+                    for i in range(stapgrootte):
+                        self.grid[row + auto.lengte - i - 1][col] = '_'
+                        row -= stapgrootte
+    
             elif richting == 'Onder':
                 if row + stapgrootte + auto.lengte - 1 < self.size and all(self.grid[row + auto.lengte + i][col] == '_' for i in range(stapgrootte)):
                     for i in range(auto.lengte):
@@ -107,6 +110,66 @@ class grid():
         # Update positie
         auto.positie = (row, col)
         
-        '''
-        Main game
-        '''
+        
+        
+def lees_csv_bestand(pad):
+    autos = []
+    with open(pad, newline='') as csvfile:
+        reader = csv.reader(csvfile)
+        next(reader)  # Sla de kopregel over
+        for row in reader:
+            naam = row[0]  # ID van de auto
+            ligging = row[1]  # 'H' of 'V'
+            col = int(row[2])
+            rij = int(row[3])
+            lengte = int(row[4])
+            autos.append(Auto(rij, col, lengte, naam, ligging))
+    return autos
+
+def kies_spelbord(mapnaam):
+    bestanden = [f for f in os.listdir(mapnaam) if f.startswith("Rushhour") and f.endswith(".csv")]
+    if not bestanden:
+        print("Geen spelborden gevonden in de map.")
+        return None
+
+    spellen = []
+    for bestand in bestanden:
+        # Splits de bestandsnaam op de underscore en haal de size en het nummer eruit
+        naam_deel = bestand.split('_')
+        if len(naam_deel) >= 2:
+            try:
+                # Verkrijg de size en spelnummer
+                size_deel = naam_deel[0].replace('Rushhour', '')  # Haal "Rushhour" weg
+                size = int(size_deel[0])  # Het eerste cijfer van de size is de gridgrootte
+                spelnummer = int(naam_deel[1].replace('.csv', ''))  # Haal het spelnummer eruit
+                spellen.append((spelnummer, size, bestand))
+            except ValueError:
+                continue  # Als de waarde geen nummer is, sla deze over
+
+    # Sorteer de spellen op spelnummer
+    spellen.sort()  # Sorteert eerst op spelnummer, dus van klein naar groot
+
+    while True:
+        try:
+            keuze = int(input(f"Kies een spelbord (1-{len(spellen)}): "))
+            if 1 <= keuze <= len(spellen):
+                gekozen_bestand = spellen[keuze - 1][2]
+                size = spellen[keuze - 1][1]
+                return os.path.join(mapnaam, gekozen_bestand), size
+            else:
+                print("Ongeldige keuze, probeer opnieuw.")
+        except ValueError:
+            print("Voer een geldig nummer in.")
+
+# Voorbeeldgebruik
+if __name__ == "__main__":
+    mapnaam = "gameboards"  # Update naar de map waar je spelborden staan
+    pad_naar_csv, size = kies_spelbord(mapnaam)
+    if pad_naar_csv:
+        autos = lees_csv_bestand(pad_naar_csv)
+
+        speelveld = Grid(size)  # Zorg ervoor dat de size wordt doorgegeven aan Grid
+        for auto in autos:
+            speelveld.toevoeg_auto(auto)
+
+        speelveld.toon_bord()
