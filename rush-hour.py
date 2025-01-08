2ce0fe93abff12dc42a40258f68227d944c25c52
Ohtaro654 <184686837+Ohtaro654@users.noreply.github.com>
2025-01-08 12:17:57 +0100

    wat comments toegevoegd

--- rush-hour.py
+++ rush-hour.py
@@ -8,10 +8,17 @@ class auto():
         self.ligging = ligging
 
 class grid():
+    '''
+    Variabele size (voor grid) toevoegen.
+    '''
     def __init__(self):
         # Maakt grid van 6x6 met _
         self.grid = [['_'] * 6 for _ in range(6)]
 
+
+        '''
+        Auto's toevoegen uit een csv file
+        '''
     def toevoeg_auto(self, auto):
         row, col = auto.positie
         if auto.ligging == 'H':
@@ -19,26 +26,32 @@ class grid():
             if col + auto.lengte > 6:
                 raise ValueError("Past niet op het bord!")
             # Kijken of niet overlapt
-            for i in range(col, col + auto.lengte):
-                if self.grid[row][i] != '_':
+            for i in range(auto.lengte):
+                if self.grid[row][col + i] != '_':
                     raise ValueError("Auto overlapt!")
             # Voeg auto toe
-            for i in range(col, col + auto.lengte):
-                self.grid[row][i] = auto.naam
+            for i in range(auto.lengte):
+                self.grid[row][col + i] = auto.naam
 
         elif auto.ligging == 'V':
             # Kijken of auto past op bord
             if row + auto.lengte > 6:
                 raise ValueError("Past niet op het bord!")
             # Kijken of niet overlapt
-            for j in range(row, row + auto.lengte):
-                if self.grid[j][col] != '_':
+            for j in range(auto.lengte):
+                if self.grid[row + j][col] != '_':
                     raise ValueError("Auto overlapt!")
             # Voeg auto toe
-            for j in range(row, row + auto.lengte):
-                self.grid[j][col] = auto.naam
+            for j in range(auto.lengte):
+                self.grid[row + j][col] = auto.naam
                 
+        '''
+        Finish maken
+        '''
                 
+    '''
+    Variabele 'stappen' toevoegen voor hoeveel stappen je wilt zetten met auto
+    '''
     def beweeg_auto(self, auto, richting):
         row, col = auto.positie
         # Ligging horizontaal
@@ -69,11 +82,22 @@ class grid():
         elif auto.ligging == 'V':
             if richting == 'Boven':
                 if row - 1 >= 0 and self.grid[row - 1][col] == '_':
+                    for i in range(auto.lengte):
+                        self.grid[row - 1 + i][col] = auto.naam
+                    self.grid[row + auto.lengte - 1][col] = '_'
                     row -= 1
+                    
             elif richting == 'Onder':
                 if row + 1 < 6 and self.grid[row + 1][col] == '_':
+                    for i in range(auto.lengte):
+                        self.grid[row + 1 + i][col] = auto.naam
+                    self.grid[row][col] = '_'
                     row += 1
             else:
                 raise ValueError("Ongeldige richting voor verticale auto!")
         # Update positie
         auto.positie = (row, col)
+        
+        '''
+        Main game
+        '''
